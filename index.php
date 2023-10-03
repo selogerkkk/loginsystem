69cb5df8717b93a6e226bbda005d6bd8745c50cd
selogerkkk <ruanhborges@gmail.com>
2023-10-03 11:53:16 -0300

    fix: form style

--- index.php
+++ index.php
@@ -5,36 +5,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+<style>
+    body {
+        background-color: #F2F2F2;
+    }
+
+    .login {
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4 offset-lg-4">
-                <div class="card">
-                    <div class="card-body">
-                        <form action="login.php" method="POST">
-                            <div>
-                                <div class="mb-3">
-                                    <label for="usuario">Usuário</label>
-                                    <input type="text" name="usuario" class="form-control">
+    <div class="login">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 offset-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3>Acesso restrito</h3>
+                            <form action="login.php" method="POST">
+                                <div>
+                                    <div class="mb-3">
+                                        <label for="user">Usuário</label>
+                                        <input type="text" name="user" class="form-control">
+                                    </div>
                                 </div>
-                            </div>
-                            <div>
-                                <div class="mb-3">
-                                    <label for="senha">Senha</label>
-                                    <input type="password" name="Senha" class="form-control">
+                                <div>
+                                    <div class="mb-3">
+                                        <label for="password">Senha</label>
+                                        <input type="password" name="password" class="form-control">
+                                    </div>
                                 </div>
-                            </div>
-                            <div>
-                                <div class="mb-3">
-                                    <button class="btn btn-primary">Entrar</button>
+                                <div>
+                                    <div class="mb-3">
+                                        <button class="btn btn-primary">Entrar</button>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
